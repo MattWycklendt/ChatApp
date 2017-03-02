014ed1b16e2a3d78673517a08b5b5716df1d49cb
mattwycklendt <mwycklendt@veriflowsystems.com>
2017-03-02 13:12:18 -0600

    added a uwsgi ini as a startup option.

--- flask_server/flask_server/flask_server.py
+++ flask_server/flask_server/flask_server.py
@@ -1,9 +1,12 @@
+from gevent import monkey
+monkey.patch_all()
+
 from flask import Flask, send_from_directory
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app, async_mode='gevent_uwsgi')
 
 @app.route('/')
 def hello_world():
@@ -11,12 +14,18 @@ def hello_world():
 
 @socketio.on('chat message')
 def handle_message(message):
-    print('[new_message] ' + message)
-    emit('chat message', message, broadcast=True)
+    print('[new_messageb] ' + message)
+    socketio.send('chat message', "msg1: " + message)
+    emit('chat message', "msg2: " + message, broadcast=True)
+    print ('emitted')
 
-@socketio.on('connection')
-def handle_connection(message):
+@socketio.on('connect')
+def handle_connect():
     print('Connected!')
+
+@socketio.on('disconnect')
+def handle_disconnect():
+    print('Disconnected!')
 
 if __name__ == '__main__':
     socketio.run(app)
--- flask_server/setup.py
+++ flask_server/setup.py
@@ -7,5 +7,5 @@ setup(
     packages=['flask_server'],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['flask', 'flask-socketio', 'gevent']
+    install_requires=['flask', 'flask-socketio', 'gevent', 'uwsgi']
 )
